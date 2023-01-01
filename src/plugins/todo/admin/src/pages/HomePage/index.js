9e6fcab4f2ab9edf9b0d22bec2b33626cc73db8d
g-umac <100878962+g-umac@users.noreply.github.com>
2023-01-01 22:28:55 +0100

    added todo plugins

--- src/plugins/todo/admin/src/pages/HomePage/index.js
+++ src/plugins/todo/admin/src/pages/HomePage/index.js
@@ -1,20 +1,79 @@
-/*
- *
- * HomePage
- *
- */
+import React, { memo, useState } from "react";
+import { nanoid } from "nanoid";
+import {
+  Layout,
+  BaseHeaderLayout,
+  ContentLayout,
+} from "@strapi/design-system/Layout";
 
-import React from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+import { EmptyStateLayout } from "@strapi/design-system/EmptyStateLayout";
+import { Button } from "@strapi/design-system/Button";
+import Plus from "@strapi/icons/Plus";
+import { Illo } from "../../components/Illo";
+
+import TodoModal from "../../components/TodoModal";
+import TodoCount from "../../components/TodoCount";
+import TodoTable from "../../components/TodoTable";
 
 const HomePage = () => {
+  const [todoData, setTodoData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  async function addTodo(data) {
+    setTodoData([...todoData, { ...data, id: nanoid(), isDone: false }]);
+  }
+
+  async function toggleTodo(data) {
+    alert("Add Toggle Todo in API");
+  }
+
+  async function deleteTodo(data) {
+    alert("Add Delete Todo in API");
+  }
+
+  async function editTodo(id, data) {
+    alert("Add Edit Todo in API");
+  }
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy Strapi coding</p>
-    </div>
+    <Layout>
+      <BaseHeaderLayout
+        title="Todo Plugin"
+        subtitle="All your todos in one place."
+        as="h2"
+      />
+
+      <ContentLayout>
+        {todoData.length === 0 ? (
+          <EmptyStateLayout
+            icon={<Illo />}
+            content="You don't have any todos yet..."
+            action={
+              <Button
+                onClick={() => setShowModal(true)}
+                variant="secondary"
+                startIcon={<Plus />}
+              >
+                Add your first todo
+              </Button>
+            }
+          />
+        ) : (
+          <>
+            <TodoCount count={todoData.length} />
+            <TodoTable
+              todoData={todoData}
+              setShowModal={setShowModal}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+              editTodo={editTodo}
+            />
+          </>
+        )}
+      </ContentLayout>
+      {showModal && <TodoModal setShowModal={setShowModal} addTodo={addTodo} />}
+    </Layout>
   );
 };
 
-export default HomePage;
+export default memo(HomePage);
